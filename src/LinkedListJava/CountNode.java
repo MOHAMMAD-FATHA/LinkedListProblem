f03bb56e0be783d2b7ecb9ff5bdd0d7f533fd79a
unknown <mfjkd7795@gmail.com>
2021-07-17 11:28:13 -0700

    Added : Create a linked list by adding 56 and 30 to 70

--- src/LinkedListJava/CountNode.java
+++ src/LinkedListJava/CountNode.java
@@ -34,6 +34,19 @@ public class CountNode {
 		}
 	}
 
+	public void push(int new_data) {
+		/*
+		 * 1 & 2: Allocate the Node & Put in the data
+		 */
+		Node new_node = new Node(new_data);
+
+		/* 3. Make next of new Node as head */
+		new_node.next = head;
+
+		/* 4. Move the head to point to new Node */
+		head = new_node;
+	}
+
 	// display() will display all the nodes present in the list
 	public void display() {
 		// Node current will point to head
@@ -43,7 +56,7 @@ public class CountNode {
 			System.out.println("List is empty");
 			return;
 		}
-		System.out.println("Nodes of singly linked list: ");
+		System.out.println("Nodes of linked list: ");
 		while (current != null) {
 			// Prints each node by incrementing pointer
 			System.out.print(current.data + " ");
@@ -57,9 +70,9 @@ public class CountNode {
 		CountNode sList = new CountNode();
 
 		// Add nodes to the list
-		sList.addNode(56);
-		sList.addNode(30);
 		sList.addNode(70);
+		sList.push(30);
+		sList.push(56);
 
 		// Displays the nodes present in the list
 		sList.display();
